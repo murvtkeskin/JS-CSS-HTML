bb1e6cda05792142de7dc87d20d57e9897b21c67
Murat Keskin <danteagreil@gmail.com>
2023-07-31 22:51:50 +0300

    update3

---
+++ Dark.Mode/script.js
@@ -0,0 +1,6 @@
+const container = document.querySelector('.container');
+const toggle = document.querySelector('.toggle');
+
+toggle.addEventListener('click', () => {
+    container.classList.toggle('dark')?(toggle.firstElementChild.className = 'far fa-moon'):(toggle.firstElementChild.className = 'far fa-sun')
+})
